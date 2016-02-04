6128bb68be5dde7ba1c598d663dbc753a84b5314
Haris Subandie Md Suhaimin <haris@vms.my>
2016-02-04 21:40:42 +0800

    cleanup

--- utilities/WG2MP/WG2MP.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-rem specify:  wg2mp inputxmltvfile outputxmltvfile
-rem inputxmltvfile and outputxmltvfile must be different!
-WG2MP C:\ProgramData\ServerCare\WebGrab\tvguide.xml C:\ProgramData\ServerCare\WebGrab\WG2MP.xml
-exit
--- utilities/xmltv_time_correct/xmltv_time_correct.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-rem specify:  xmltv_time_correct inputxmltvfile outputxmltvfile
-rem inputxmltvfile and outputxmltvfile must be different!
-xmltv_time_correct xmltv.xml tvguide.xml
-exit
